b0d731a0a5870566456d46dc23bdbb727fd30f32
na-yk <ninghao17@naver.com>
2022-11-18 19:54:09 +0900

    chore: Modify routes

--- routes/index.js
+++ routes/index.js
@@ -16,10 +16,10 @@ router.post('/login', userController.login);
 // router.get('/login', userController.getUser);
 
 // 내 스노우볼 관련 API
-router.get('/mysnowball', mySbController.getSnowball);
+router.get('/mysnowball/:ownerId', mySbController.getSnowball);
 router.get('/mysnowball/:ownerId/messages',mySbController.getAllMsg);
 router.get('/mysnowball/:ownerId/voices', mySbController2.getVoices);
-router.get('/mysnowball/:ownerId/messages/message', mySbController2.getOneMsg);
+router.get('/mysnowball/:ownerId/messages', mySbController2.getOneMsg);
 
 
 
