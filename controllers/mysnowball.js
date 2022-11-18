6504e666573352befb70d6bac7170a739f56fc28
na-yk <ninghao17@naver.com>
2022-11-18 19:51:41 +0900

    feat Add getSnowball, getAllObjects

--- controllers/mysnowball.js
+++ controllers/mysnowball.js
@@ -3,26 +3,71 @@ const prisma = new PrismaClient()
 
 // GET 'api/mysnowball/:ownerId' 사용자가 가진 오브제 리스트 반환
 const getSnowball = async (req, res) => {
-    try {
-        const {ownerId} = req.params.user_id;
-        // ownerId 에 해당하는 사람 객체 불러오기
-        const user = prisma.user.findUnique({where: { ownerId:ownerId }});
-        // ownerId 에 해당하는 사람의 스노우볼 메시지 모두 불러오기
-        res.json({ok: true, user: user.ownerId});
-        // const messages = await prisma.message.findUnique({where: { messageId: ownerId }});
+    const ownerId = parseInt(req.params.ownerId);
+    
+    const receiver = await prisma.User.findUnique({
+        where:{
+            ownerId : ownerId
+        },
+        select : {
+            name: true,
+            cnt : true,
+        },
+    });
 
-        // 메시지 모두 보내기
-        
+    const objets = await prisma.Message.findMany({
+        where : {
+            receiverId : ownerId,
+        },
+        select : {
+            writer : true,
+            top: true,
+            left: true,
+            Objet : true 
+        }
+    });
 
-    } catch(err) {
-        // res.send(ownerId)
-        res.status(err.statusCode).json({ message : err.message });
-    }
+    
+
+    return res.status(200).send({
+        "name" : receiver.name,
+        "messages" : objets
+    });
 }
+
+
 
 // // GET '/api/mysnowball/:ownerId/messages'
 const getAllMsg = async (req,res) => {
+    const ownerId = parseInt(req.params.ownerId); 
 
+    
+    const receiver = await prisma.User.findUnique({
+        where:{
+            ownerId : ownerId
+        },
+        select : {
+            name: true
+        },
+    });
+    // console.log(receiver.name);
+
+    const messages = await prisma.Message.findMany({
+        where : {
+            receiverId : ownerId,
+        },
+        select:{
+            messageId : true,
+            writer: true,
+            comment: true,
+            personalVoice: true
+        }
+    })
+
+    return res.status(200).send({
+        "name" : receiver.name,
+        "messages" : messages
+    });
 }
 
 
