d4e5c502346b3b65977bc88c985739453ac75467
na-yk <ninghao17@naver.com>
2022-11-19 10:04:37 +0900

    chore: Modify routes for getVoices

--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,8 @@ router.post('/login', userController.login);
 // 내 스노우볼 관련 API
 router.get('/mysnowball/:ownerId', mySbController.getSnowball);
 router.get('/mysnowball/:ownerId/messages',mySbController.getAllMsg);
-router.get('/mysnowball/:ownerId/voices', mySbController2.getVoices);
-router.get('/mysnowball/:ownerId/messages', mySbController2.getOneMsg);
-
-
-
-
+router.get('/mysnowball/:ownerId/merryChristmas', mySbController2.getVoices);
+router.get('/mysnowball/:ownerId/messages/message', mySbController2.getOneMsg);
 
 // 남의 스노우볼 관련 API
 router.get('/:ownerId/objets', yourSbController.getAllObjets);
