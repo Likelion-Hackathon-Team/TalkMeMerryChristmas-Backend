89128c59dbe70e1b1749d7ffe9052b5c107897e4
na-yk <ninghao17@naver.com>
2022-11-19 14:08:37 +0900

    chore: Set helmet and compression for heroku deployment

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const bodyParser = require('body-parser')
 const app = express()
 const routes = require('./routes')
 const cors = require('cors')
+const helmet = require('helmet');
+const compression = require('compression');
 
 app.use(cors({
   origin: '*', // 모든 출처 허용 옵션. true 를 써도 된다.
 }));
+app.use(compression());
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
