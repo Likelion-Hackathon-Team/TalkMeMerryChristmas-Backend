728dca7d691e0ee2821f3df471b88e383d887a05
na-yk <ninghao17@naver.com>
2022-11-19 10:03:12 +0900

    feat: Create getAllObjets, postMessage

--- controllers/yoursnowball.js
+++ controllers/yoursnowball.js
@@ -14,42 +14,46 @@ const getAllObjets = async (req, res) => {
 
 // POST 
 const postMessage = async (req, res) => {
-    // try { 
-        const { writer, comment, objetId, top, left } = req.body;
-        const receiverId = parseInt(req.params.ownerId); 
-        
-        console.log(req.files)
+    const { writer, comment, objetId, top, left } = req.body;
+    const receiverId = parseInt(req.params.ownerId); 
+    
+    // console.log(req.files)
 
-        //S3에 업로드된 파일 주소 저장하기
-        const commonVoice = req.files['commonVoice'][0].location;
-        const personalVoice = req.files['personalVoice'][0].location;
-        
+    //S3에 업로드된 파일 주소 저장하기
+    const commonVoice = req.files['commonVoice'][0].location;
+    const personalVoice = req.files['personalVoice'][0].location;
+    
 
-        const newMessage = await prisma.Message.create({
-            data:{
-                receiverId: receiverId,
-                writer,
-                comment,
-                commonVoice:commonVoice,
-                personalVoice:personalVoice,
-                objetId: parseInt(objetId),
-                top: parseInt(top),
-                left: parseInt(left)
+    const newMessage = await prisma.Message.create({
+        data:{
+            receiverId: receiverId,
+            writer,
+            comment,
+            commonVoice:commonVoice,
+            personalVoice:personalVoice,
+            objetId: parseInt(objetId),
+            top: parseInt(top),
+            left: parseInt(left)
+        }
+    })
+    
+    const updateUser = await prisma.User.update({
+        where: {
+            ownerId: receiverId,
+        },
+        data: {
+            cnt: {
+                increment: 1,
             }
-        })
-        
-        
-        
-        return res.status(200).send([{
-            "newMesssageWriter" : newMessage.writer,
-            "newMessageComment" : newMessage.comment
-        }]);
-    // } catch(err) {
-    //     res.status(err.statusCode).json({ "message" : err.message });
-    // }
+        },
+    })
+    
+    return res.status(200).json({
+        "writer" : newMessage.writer,
+        "comment" : newMessage.comment,
+        "cnt" : updateUser.cnt
+    });
 };
-
-
 
 //export controller functions
 module.exports = {
